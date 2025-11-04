7eae07a5268ac28ffb30248bcf9b87b7135456a2
FlavioMatias <flaviomatiasnf@gmail.com>
2025-11-04 16:44:13 -0300

    feat: fix repositories imports

--- appointment/src/main/java/medical/api/appointment/repository/AppointmentRepository.java
+++ appointment/src/main/java/medical/api/appointment/repository/AppointmentRepository.java
@@ -1,2 +1,8 @@
+package medical.api.appointment.repository;
+
+import medical.api.appointment.model.Appointment;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
 @Repository
 public interface AppointmentRepository extends JpaRepository<Appointment, Long>{}
--- appointment/src/main/java/medical/api/appointment/repository/DoctorRepository.java
+++ appointment/src/main/java/medical/api/appointment/repository/DoctorRepository.java
@@ -1,2 +1,8 @@
+package medical.api.appointment.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import medical.api.appointment.model.Doctor;
+
 @Repository
-public interface DoctorRepository extends JpaRepository<Doctor, Long>{}
+public interface DoctorRepository extends JpaRepository<Doctor, Long> {}
